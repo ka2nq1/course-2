e6e7484ebecb2c4352a0afe589088c9cfb7b50d4
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-03-16 17:19:04 +0200

    adds desktop program

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const resultsItem = document.getElementsByClassName('programAcordeonItem');
-const programBtn = document.getElementsByClassName("btn");
 
 for (let i = 0; i < resultsItem.length; i++) {
     resultsItem[i].addEventListener("click", function() {
         const dropdown = this.querySelector('.programDropdown');
         console.log(dropdown)
         this.classList.toggle("active");
-        // dropdown.classList.toggle("active");
-        
-        // var panel = this.nextElementSibling;
-        // if (panel.style.display === "block") {
-        //     panel.style.display = "none";
-        // } else {
-        //     panel.style.display = "block";
-        // }
     });
 }
 
