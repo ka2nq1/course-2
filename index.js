1e6cbe60261899b2f5d33de2835e797a3b8473ae
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-03-17 17:50:03 +0200

    fixes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ for (let i = 0; i < resultsItem.length; i++) {
     resultsItem[i].addEventListener("click", function() {
         this.classList.toggle("active");
     });
+
+    console.log(resultsItem.filter((e, index) => index !== i))
 }
 
 for (let i = 0; i < questionsItem.length; i++) {
