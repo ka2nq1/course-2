e8bd98b439298c97b8941f4990141f5fc54933fa
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-03-17 18:09:05 +0200

    fixes

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const resultsItem = document.getElementsByClassName('programAcordeonItem');
-const questionsItem = document.getElementsByClassName('questionsItem');
 
 for (let i = 0; i < resultsItem.length; i++) {
     resultsItem[i].addEventListener("click", function() {
+        for (let j = 0; j < resultsItem.length; j++) {
+            if (i !== j) {
+                resultsItem[j].classList.remove("active");
+            }
+        }
         this.classList.toggle("active");
     });
-
-    console.log(resultsItem.filter((e, index) => index !== i))
 }
+
+const questionsItem = document.getElementsByClassName('questionsItem');
 
 for (let i = 0; i < questionsItem.length; i++) {
     questionsItem[i].addEventListener("click", function() {
+        for (let j = 0; j < questionsItem.length; j++) {
+            if (i !== j) {
+                questionsItem[j].classList.remove("active");
+            }
+        }
         this.classList.toggle("active");
     });
 }
