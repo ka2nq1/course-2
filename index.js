6d8cd347d4a6bbe90820f77d3bb8789d2abbb9c5
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-03-17 16:41:21 +0200

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const resultsItem = document.getElementsByClassName('programAcordeonItem');
+const questionsItem = document.getElementsByClassName('questionsItem');
 
 for (let i = 0; i < resultsItem.length; i++) {
     resultsItem[i].addEventListener("click", function() {
-        const dropdown = this.querySelector('.programDropdown');
-        console.log(dropdown)
+        this.classList.toggle("active");
+    });
+}
+
+for (let i = 0; i < questionsItem.length; i++) {
+    questionsItem[i].addEventListener("click", function() {
         this.classList.toggle("active");
     });
 }
